7e1e88c7d533161ff4cf40e4192d071cdb2c600c
stackoverthrow6 <stackoverthrow6@gmail.com>
2019-03-01 02:49:29 -0800

    finished early

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -4,7 +4,20 @@ import { connect } from 'react-redux'
 class LeaderBoard extends Component {
     render(){
         return(
-            <div className='center'>LEADERBOARD</div>
+            <div className='center'>
+            {this.props.users.sort((a,b) =>
+                (Object.values(b.answers).length+b.questions.length) - (Object.values(a.answers).length+a.questions.length)
+            )
+            .map((user) => 
+               <div key={user.id}>
+                    <div>{user.name}</div>
+                    <div>{"Answered questions : "+Object.values(user.answers).length}</div>
+                    <div>{"Created questions : "+user.questions.length}</div>
+                    <div>{"Score : "+(Object.values(user.answers).length+user.questions.length)}</div>
+                    <p></p>
+               </div> 
+                )}
+            </div>
         )
     }
 }
